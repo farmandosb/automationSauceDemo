3d1ec71b37057ec2bdb8c97eff3999bde7488e74
farmandosb <freddy.armando.suarez@gmail.com>
2021-11-18 08:52:35 -0300

    Initial commit.

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+    public static String greeting = "Hello world";
+    public static void main(String[] args) {
+
+        System.out.println(greeting);
+    }
+}
---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class ApplicationTest {
+    @Test
+    public void test01() throws InterruptedException {
+        Assert.assertEquals(Application.greeting,"Hello world");
+        System.setProperty("webdriver.chrome.driver","./chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://www.saucedemo.com/");
+        Thread.sleep(2000);
+        //driver.close();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //WebElement inputUsername = driver.findElement(By.id("user-name"));
+        WebElement inputUsername = driver.findElement(By.xpath("//div[@class='login-box']//input[@id='user-name']"));
+        inputUsername.clear();
+        inputUsername.sendKeys("standard_user");
+
+        WebElement inputPassword = driver.findElement(By.xpath("//div[@class='login-box']//input[@id='password']"));
+        inputPassword.clear();
+        inputPassword.sendKeys("secret_sauce");
+
+        WebElement btnLogin = driver.findElement(By.xpath("//div[@class='login-box']//input[@id='login-button']"));
+        btnLogin.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://www.saucedemo.com/inventory.html");
+        driver.close();
+    }
+}
